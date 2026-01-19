e14d66b359e5da7f2e9174b693eabe05762d6020
Noopsiyo <ethandamian.saenz@gmail.com>
2026-01-19 12:23:56 -0600

    gitca add stage"1"

--- src/main.c
+++ src/main.c
@@ -1,7 +1,35 @@
 #include <stdio.h>
 
-int main(int argc, char *argv)
-{
-    printf("Hello world!\n");
-    return 0;
+int main(){
+
+    float lado, area, volumen;
+
+    printf("===========================================================\n");
+    printf("                CALCULO DE AREA Y VOLUMEN\n");
+    printf("===========================================================\n\n");
+     
+    printf("ingrese valor de lado:");
+    scanf("%f", &lado);
+
+    if(lado <= 0)
+    {printf("\nError: el lado debe ser positivo.\n");
+        return 1;
+
+}
+area = lado * lado;
+
+volumen = lado * lado *lado;
+
+
+
+printf ("\n--------------------------------------------");
+printf ("RESULTADOS:\n");
+printf("--------------------------------------------\n");
+printf("lado ingresado: %2f\n", lado);
+printf("area del cuadrado: %.2f unidades cuadradas\n", area);
+printf("volumen de cubo: %.2f unidades cubicas\n", volumen);
+printf("----------------------------------\n");
+
+return 0;
+
 }
